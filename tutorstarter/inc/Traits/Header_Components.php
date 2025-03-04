faaf61f0ae233f129c3d1b4d41965b6904ccb593
Sean Murphy <sean@iamseanmurphy.com>
2025-03-04 10:17:59 +0000

    3.0.2

--- tutorstarter/inc/Traits/Header_Components.php
+++ tutorstarter/inc/Traits/Header_Components.php
@@ -35,7 +35,7 @@ trait Header_Components {
 		$current_user  = wp_get_current_user();
 		?>
 		<div class="tutor-header-profile-photo">
-			<?php 
+			<?php
 			$profile_pic = tutor_utils()->get_tutor_avatar( get_current_user_id() );
 			?>
 		</div><!-- .tutor-header-profile-photo -->
